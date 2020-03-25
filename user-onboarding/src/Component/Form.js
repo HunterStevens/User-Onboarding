094ebdb80dd49052b6b35285b5e3c9539636c2fb
Hunter Stevens <hunterstevens1996@gmail.com>
2020-03-25 16:46:52 -0600

    Most of the way done. Need to work on the Validation messages as it keeps bringing errors.

--- user-onboarding/src/Component/Form.js
+++ user-onboarding/src/Component/Form.js
@@ -5,9 +5,9 @@ import axios from 'axios';
 const schema = yup.object().shape({
     firstName:yup.string().required('First Name is required.'),
     lastName:yup.string().required("Last Name is required."),
-    email:yup.string().email("Enter a valid email address."),
-    password:yup.string().required("a password is required.").min(4).matches(/(^(?=.*[!@#$%^&*()-_=+<>/])) /, "Your Password is required to have special characters with it (at least one)"),
-    vacation:yup.string().required("Choose your dream vactaion"),
+    email:yup.string().email("Enter a valid email address.").required("Can't leave Email field blank"),
+    password:yup.string().required("a password is required."),//.min(4).matches(/(^(?=.*[!@#$%^&*()-_=+<>/])) /, "Your Password is required to have special characters with it (at least one)"),
+    vacation:yup.string(),//.required("Choose your dream vactaion"),
     terms: yup.boolean().oneOf([true], "Agree to the terms and conditions that have yet to be displayed... Still working on it.")
 });
 
@@ -16,53 +16,113 @@ function Form(){
     const[submitDisabled, setSubmitDisabled] = useState(true);
 
     const [formInput, setFormInput] = useState({
-        firstName: '',
-        lastName: '',
-        email: '',
-        password: '',
-        vacation: '',
+        firstName:'',
+        lastName:'',
+        email:'',
+        password:'',
+        vacation:'',
+        terms:''
     }) 
+    
+    const [formErr, setFormErr] = useState({
+        firstName:"",
+        lastName:"",
+        email:"",
+        password:"",
+        vacation:"",
+        terms:""
+    })
 
+    const[account, setAccount] = useState([]);
+
+    useEffect(() => {
+        schema.isValid(schema).then(good =>{
+            setSubmitDisabled(!good);
+        })
+    }, [formInput]);
 
     const formSubmission =(event) =>{
         event.preventDefault();
+        axios.post("https://reqres.in/api/users", formInput)
+        .then(res =>{
+            console.log("Grabbed Data", res);
+            setAccount([
+                ...account,
+                res.data
+            ])
+            console.log("New Account ", account)
+        })
+        .catch(err => console.log("ERROR: ", err));
+    }
+
+    const validateInput = event =>{
+        yup
+        .reach(schema, event.target.name)
+        .validate(event.target.value)
+        .then(good =>{
+            setFormErr({
+                ...formErr,
+                [event.target.name]:""
+            });
+        })
+        .catch(err => {
+            setFormErr({
+                ...formErr,
+               [event.target.name]:err.formErr
+            });
+        });
     }
 
 
     const formChange = (event) =>{
         event.persist();
-        
-    }
+        const newForm ={
+            ...formInput,
+            [event.target.name]: 
+            event.target.type === "checkbox" ? event.target.checked :event.target.value
+        };
+        validateInput(event);
+        setFormInput(newForm);
+    };
 
 
 return(
     <form onSubmit={formSubmission} >
         <label htmlFor="firstName">First Name: 
-            <input id="firstName" name="firstName" type="text" value={formInput.firstName} onChange={formChange}/> {/*value={} onChange={}*/} 
+            <input id="firstName" name="firstName" type="text" 
+            value={formInput.firstName} onChange={formChange}/>
+            {formErr.firstName.length > 0 ? <p>{formErr.firstName}</p> : null} 
         </label>
 
         <br/>
 
         <label htmlFor="lastName">Last Name: 
-            <input id="lastName" name="lastName" type="text" value={formInput.lastName} onChange={formChange}/> {/*value={} onChange={}*/} 
+            <input id="lastName" name="lastName" type="text"
+            value={formInput.lastName} onChange={formChange}/>
+            {formErr.lastName.length > 0 ? <p>{formErr.lastName}</p> : null}
         </label>
 
         <br/>
 
         <label htmlFor="email">Email: 
-            <input id="email" name="email" type="text" value={formInput.email} onChange={formChange} />{/*value={} onChange={}*/}
+            <input id="email" name="email" type="text" 
+            value={formInput.email} onChange={formChange} />
+            {formErr.email.length > 0 ? <p>{formErr.email}</p> : null}
         </label>
 
         <br/>
 
         <label htmlFor="password">Password: 
-            <input id="password" name="password" type="text" value={formInput.password} onChange={formChange}/> {/*value={} onChange={}*/} 
+            <input id="password" name="password" type="text" 
+            value={formInput.password} onChange={formChange}/>
+            {formErr.password.length > 0 ? <p>{formErr.password}</p> : null}
         </label>
 
         <br/>
 
-        <label htmlFor="Dream Vacation">Dream Vacation: 
-            <select id='Dream Vacation' name="dream vacation" value={formInput.vacation} onChange={formChange}>
+        <label htmlFor="vacation">Dream Vacation: 
+            <select id='vacation' name="vacation" 
+            value={formInput.vacation} onChange={formChange}>
                 <option value="Empty"> </option>
                 <option value="france">France</option>
                 <option value="italy">Italy</option>
@@ -71,13 +131,16 @@ return(
                 <option value="australia">Australia</option>
                 <option value="moon">Moon</option>
             </select>
+            {formErr.vacation.length > 0 ? <p>{formErr.vacation}</p> : null}
         </label>
 
         <br/>
 
         <label htmlFor="terms">
-            <input id="terms" name="terms" type="checkbox" checked={formInput.terms} onChange={formChange}/> {/*value={} onChange={}*/}
-            Terms and Conditions 
+            <input id="terms" name="terms" type="checkbox" 
+            checked={formInput.terms} onChange={formChange}/> 
+            Terms and Conditions
+            {formErr.terms.checked === true ? <p>{formErr.terms}</p> : null} 
         </label>
 
         <br/>
